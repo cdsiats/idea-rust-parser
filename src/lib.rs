b194877946599f13b999e82030b38ed30312fa6e
Darryl <118641404+cdsiats@users.noreply.github.com>
2025-04-29 10:01:55 +0800

    modify .toml config

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+mod tokens;
+mod lexer;
+mod tests;
+
+pub fn parse() {
+  println!("Hello, Parser!")
+}
+
+pub fn compile() {
+  println!("Hello, Compiler!")
+}
--- src/main.rs
+++
@@ -1,6 +0,0 @@
-mod tokens;
-mod lexer;
-mod tests;
-fn main() {
-    println!("Hello, world!");
-}
